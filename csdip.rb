a69992ec422d007d380a685f8aa8c43f83b8fbf0
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-15 14:47:54 +0200

    Brew formula update for csdip version v1.15.4

--- csdip.rb
+++ csdip.rb
@@ -3,16 +3,16 @@ require_relative "./lib/custom_download_strategy.rb"
 class Csdip < Formula
   desc "CSDIP keygen"
   homepage ""
-  version "1.15.3"
+  version "1.15.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/mklimuk/csdip/releases/download/v1.15.3/csdip_darwin_amd64.tar.gz", :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "1087be010d4be6e45a553055fb85710d98e321db0a61c24d8b394034d0ba3d82"
+    url "https://github.com/mklimuk/csdip/releases/download/v1.15.4/csdip_darwin_amd64.tar.gz", :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "e39f63b96ecc736c6c9e286c88517c82500457c85c21b0d0384329bb9ebd0419"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/mklimuk/csdip/releases/download/v1.15.3/csdip_linux_amd64.tar.gz", :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "ed3daaa77de729fda0b742346c868c5646b057e57e083342a0290c10155ded3f"
+      url "https://github.com/mklimuk/csdip/releases/download/v1.15.4/csdip_linux_amd64.tar.gz", :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 "a3b6616be7edf174c5b4c75164ba88426fa9db59007648229e785388d18c59ab"
     end
   end
 

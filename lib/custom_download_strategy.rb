0014a11658afe69ac7070d63e3799bf203be2997
Michal <michal.klimuk@gmail.com>
2021-07-30 10:29:31 +0200

    Update custom_download_strategy.rb

--- lib/custom_download_strategy.rb
+++ lib/custom_download_strategy.rb
@@ -107,7 +107,7 @@ class CustomGitHubPrivateRepositoryReleaseDownloadStrategy < CustomGitHubPrivate
   end
 
   def resolve_asset_id
-    release_metadata = fetch_release_metadata
+    release_metadata = get_release, @owner, @repo, @tag
     assets = release_metadata["assets"].select { |a| a["name"] == @filename }
     raise CurlDownloadStrategyError, "Asset file not found." if assets.empty?
 
